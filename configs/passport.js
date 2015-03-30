2874aba38a0d56aab20ca35570231adc37aad7b1
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2015-03-30 20:36:27 +0200

    adding the passport local signup strategy

--- configs/passport.js
+++ configs/passport.js
@@ -6,9 +6,9 @@ var User = require('../server/models/user');
 module.exports = function(passport) {
 
 	/*
-			Passport sesstion setup is required to persist login sessions
-			We need to serialize and de-serialize users
-	 */
+		Passport sesstion setup is required to persist login sessions
+		We need to serialize and de-serialize users
+	*/
 	passport.serializeUser(function(user, done) {
 		done(null, user.id);
 	});
@@ -21,37 +21,39 @@ module.exports = function(passport) {
 
 	passport.use('signup', new LocalStrategy({
 
-			// The default local strategy uses firstname and lastname, we want to use the email instead
-			usernameField: 'email',
-			passwordField: 'password',
-			passReqToCallback: true
+		// The default local strategy uses firstname and lastname, we want to use the email instead
+		usernameField: 'email',
+		passwordField: 'password',
+		passReqToCallback: true
 
-		},
-		function(req, email, password, done) {
+	}, function(req, email, password, done) {
 
-			process.nextTick(function() {
-				//  Check if the user already exists
-				User.findOne({
+		process.nextTick(function() {
+			//  Check if the user already exists
+			User.findOne({
 
-					'local.email': email
+				'local.email': email
 
-				}, function(err, user) {
-					// Return if any error happens
-					if (err) return done(err);
-					// Check to see if the email used already exists
-					if (user) { return done(null, false, req.flash('signupMessage', 'Sorry ! Apprently there is already a registered user with this email address'));
-					} else {
-						// Things look good .. create and add the user
-						var newUser = new User();
-						newUser.local.email = email;
-						newUser.local.password = newUser.generateHash(password);
+			}, function(err, user) {
+				// Return if any error happens
+				if (err) return done(err);
+				// Check to see if the email used already exists
+				if (user) {
+					return done(null, false, req.flash('signupMessage', 'Sorry ! Apprently there is already a registered user with this email address'));
+				} else {
+					// Things look good .. create and add the user
+					var newUser = new User();
+					newUser.local.email = email;
+					newUser.local.password = newUser.generateHash(password);
 
-						newUser.save(function(err) {
-							if (err) throw err;
-							return done(null, newUser);
-						});
-					}
-				});
+					newUser.save(function(err) {
+						if (err) throw err;
+						return done(null, newUser);
+					});
+				}
 			});
-		}));
+		});
+	}));
+
+
 };
