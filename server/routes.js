0b7be4462baa4d1f01ebf57090cee5b90bd5e73b
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2015-03-30 14:35:08 +0200

    adding a basic router to the index homepage

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,25 @@
-module.exports = function(app, passport) {
+var _  = require('underscore');
 
-    /* =====================================
-     *               HomePage
-     * ===================================== */
-    app.get('/', function(req, res) {
-        res.render('index.ejs'); // load the index.ejs file
+var routes = [
+    {
+        path: '/*',
+        httpMethod: 'GET',
+        middleware: [function(req, res) {
+            res.render('index');
+        }]
+    }
+];
+
+module.exports = function(app) {
+    _.each(routes, function(route) {
+        var args = _.flatten([route.path, route.middleware]);
+
+        switch(route.httpMethod.toUpperCase()) {
+            case 'GET': app.get.apply(app, args); break;
+            case 'POST': app.post.apply(app, args); break;
+            case 'PUT': app.put.apply(app, args); break;
+            case 'DELETE': app.delete.apply(app, args); break;
+            default: throw new Error('Invalid HTTP method specified for route ' + route.path); break;
+        }
     });
-
-};
+}
